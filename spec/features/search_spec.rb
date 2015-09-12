be679f9b98e47068009bdf35fd24e18ccaabee88
Rafael Costa <rafaelmbcosta@gmail.com>
2015-09-11 21:49:09 -0300

    [Finish #103238730] suggestion screen

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,11 +1,11 @@
 #encoding: utf-8
 require "rails_helper"
+require 'capybara/rspec'
 
 describe "View screen" do
   scenario "When entering the search screen I expect to see 'Dicionário'" do
     visit "/"
     expect(page).to have_text("Dicionário")
   end
-
 end
 
